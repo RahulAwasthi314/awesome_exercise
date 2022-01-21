a7067c736744c567c40d438016d57cb80b6bd5a7
Rahul Awasthi <awasthir@pm.me>
2022-01-21 23:24:23 +0530

    Completed exercise 2

--- c/main.cpp
+++ c/main.cpp
@@ -1,15 +1,24 @@
 // create an array of size 10 
 
 #include <iostream>
+#include <iomanip>
+
+#define RAND_LIMIT 100
+#define MAX_VALUE 1000
+
 using namespace std;
+
 int main () {
     double arr[1000] = {0};
+    for (int i = 0; i < MAX_VALUE; i++) {
+        arr[i] = rand() % RAND_LIMIT;
+    }
     for (int i = 0; i < 1000; i++) {
         if (i % 10 == 0) {
             cout << endl;
-            cout << i + 10 << " ";
+            cout << setw(4) << i + 10 << setfill(' ') << "   ";
         }
-        cout << arr[i] << " "; 
+        cout << setw(2) << arr[i] << setfill(' ') << " ";
     }
     return 0;
 }
