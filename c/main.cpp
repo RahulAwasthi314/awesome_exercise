64cd92fd55fd64f233f6366b8be7b0c70a9ee2cd
Rahul Awasthi <awasthir@pm.me>
2022-01-21 21:45:28 +0530

    Completed exercise 1

---
+++ c/main.cpp
@@ -0,0 +1,15 @@
+// create an array of size 10 
+
+#include <iostream>
+using namespace std;
+int main () {
+    double arr[1000] = {0};
+    for (int i = 0; i < 1000; i++) {
+        if (i % 10 == 0) {
+            cout << endl;
+            cout << i + 10 << " ";
+        }
+        cout << arr[i] << " "; 
+    }
+    return 0;
+}
