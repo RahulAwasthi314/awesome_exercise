b5e90fa08c83b30287c792ac95c98884d0db8b59
Rahul Awasthi <awasthir@pm.me>
2022-01-23 01:06:04 +0530

    Corrected ex_01_main.cpp

--- cpp/ex_01_cpp/ex_01_main.cpp
+++ cpp/ex_01_cpp/ex_01_main.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 #include <iomanip>
-
-#define RAND_LIMIT 100
 #define MAX_VALUE 1000
 
 using namespace std;
 
 int main () {
     double arr[1000] = {0};
-    // assigning the random values to array 
-    for (int i = 0; i < MAX_VALUE; i++) {
-        arr[i] = rand() % RAND_LIMIT;
-    }
+
     // printing the values to verify
     for (int i = 0; i < 1000; i++) {
         if (i % 10 == 0) {
