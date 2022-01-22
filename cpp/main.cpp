1391936898c6b342f5cd1698487b275196caa646
Rahul Awasthi <awasthir@pm.me>
2022-01-22 14:16:06 +0530

    Completed exercise one

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main () {
-    cout << "Hello, world!\n";
+    double arr[1000] = {0};
+    for (int i = 0; i < 1000; i++) {
+        if (i % 10 == 0) {
+            cout << endl;
+            cout << i + 10 << " ";
+        }
+        cout << arr[i] << " "; 
+    }
     return 0;
 }
