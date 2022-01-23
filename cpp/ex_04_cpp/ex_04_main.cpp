38e0edef34ed1ff49eb5f27438989c7f4e3a329e
Rahul Awasthi <awasthir@pm.me>
2022-01-23 17:00:41 +0530

    Implemented quick sort

--- cpp/ex_04_cpp/ex_04_main.cpp
+++ cpp/ex_04_cpp/ex_04_main.cpp
@@ -18,6 +18,9 @@ double avg(const double*);
 void mergeSort(double*, int, int);
 void merge(double*, int, int, int);
 
+void swap(double*, int, int);
+int partition(double*, int, int);
+void quickSort(double*, int, int);
 
 int main () {
     srand(time(NULL));
@@ -45,12 +48,13 @@ int main () {
     res = avg(arr);
     cout << "Average value in the array is: " << res << endl;
 
-
-    mergeSort(arr, 0, MAX_VALUE - 1);
+    double ar[] = {5,4,3,2,1};
+    quickSort(ar, 0, 4);
+    // mergeSort(arr, 0, MAX_VALUE - 1);
 
     cout << "Sorted array is: ";
-    for (int i = 0; i < MAX_VALUE; i++) {
-        cout << arr[i] << " ";
+    for (int i = 0; i < 5; i++) {
+        cout << ar[i] << " ";
     }
     cout << endl;
     return 0;
@@ -127,5 +131,38 @@ void merge(double* arr, int p, int q, int r) {
     }
     while (j < m) {
         arr[k++] = b[j++];
+    }
+}
+
+/*
+ * Quick Sort
+// Time Complexity: O(n log n) {avg case}
+ */
+
+void swap(double* arr, int i, int j) {
+    double t = *(arr + i);
+    *(arr + i) = *(arr + j);
+    *(arr + j) = t;
+}
+
+// partition logic
+// Time Complexity : O(n)
+int partition(double* arr, int p, int r) {
+    int i = p - 1;
+    double x = arr[r];
+    for (int j = p; j < r; j++) {
+        if (arr[j] <= x) swap(arr, ++i, j);
+    }
+    swap(arr, i+1, r);
+    return i+1;
+}
+
+// quickSort 
+// Time Complexity: O(log n)
+void quickSort(double* arr, int p, int r) {
+    if (p < r) {
+        int q = partition(arr, p, r);
+        quickSort(arr, p, q - 1);
+        quickSort(arr, q + 1, r);
     }
 }
